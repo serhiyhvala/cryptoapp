c0b8c7369fafcd568fb17b9dc02558452ab1318d
Serhiy Hvala <serhiyhvaladew@gmail.com>
2023-02-24 14:57:18 +0200

    Create Layout Of Project

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,49 @@
+import { Layout, Space, Typography } from 'antd'
+import { Link, Route, Routes } from 'react-router-dom'
+
+import './App.css'
+import {
+	CryptoDetails,
+	Cryptocurrencies,
+	Exchanges,
+	Homepage,
+	Navbar,
+	News
+} from './components'
+
 const App = () => {
 	return (
-		<div>
-			<h1>Cryptoverse</h1>
+		<div className='app'>
+			<div className='navbar'>
+				<Navbar />
+			</div>
+			<div className='main'>
+				<Layout>
+					<div className='routes'>
+						<Routes>
+							<Route path='/' element={<Homepage />} />
+							<Route path='/exchanges' element={<Exchanges />} />
+							<Route path='/cryptocurrencies' element={<Cryptocurrencies />} />
+							<Route path='/crypto/:coinId' element={<CryptoDetails />} />
+							<Route path='/news' element={<News />} />
+						</Routes>
+					</div>
+				</Layout>
+				<div className='footer'>
+					<Typography.Title
+						level={5}
+						style={{ color: 'white', textAlign: 'center' }}
+					>
+						Cryptoverse <br />
+						All rights reserved
+					</Typography.Title>
+					<Space>
+						<Link to='/'>Home</Link>
+						<Link to='/exchanges'>Exchanges</Link>
+						<Link to='/news'>News</Link>
+					</Space>
+				</div>
+			</div>
 		</div>
 	)
 }
